3fc75c2bdd275cf6136ec2821c0e5273b70cd426
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2022-01-10 16:52:48 -0600

    commit tree fe26c121e42888c97e864eccd935d3dceed7def6

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,4 +4,12 @@ class Student < ApplicationRecord
     "#{first_name} #{last_name}"
   end
 
+  def self.all_by_grade
+    all.order(grade: "DESC")
+  end
+
+  def self.highest_grade
+    all_by_grade.first
+  end
+
 end
